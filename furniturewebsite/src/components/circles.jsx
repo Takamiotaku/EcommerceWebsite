4adbf314a625b67dd541b93d7dc99c50163a7c24
Takamiotaku <chlupkasebastian@gmail.com>
2024-03-14 02:07:31 +0000

    basket,account,delivery

--- furniturewebsite/src/components/circles.jsx
+++ furniturewebsite/src/components/circles.jsx
@@ -1,17 +1,31 @@
-//Give the component access to its CSS
-import "./styles.css"
+import Account from "./Account";
+import Basket from "./Basket";
+import Delivery from "./Delivery"
 
-//functional component - must be Capitalised
-export default function Circles(props){
-    
-    // Variables and functions go here
-    
-    
-    //HTML goes in the return.
-    return(
-        <div className="circles">
-            <p>{[props.name]}</p>
+// Update Circles component to accept a prop indicating which component to render
+export default function Circles(props) {
+    // Variable to hold the component to render
+    let componentToRender;
+
+    // Determine which component to render based on the prop value
+    switch (props.name) {
+        case "Account":
+            componentToRender = <Account/>;
+            break;
+        case "Basket":
+            componentToRender = <Basket/>;
+            break;
+        case "delivery":
+            componentToRender = <Delivery/>;
+            break;
+        default:
+            componentToRender = null;
+    }
+
+    // Render the determined component
+    return (
+        <div className="circles" onClick={() => props.onClick(componentToRender)}>
+            <p>{props.name}</p>
         </div>
-    )
+    );
 }
-
--- furniturewebsite/src/components/showcase.jsx
+++ furniturewebsite/src/components/showcase.jsx
@@ -1,39 +1,39 @@
-//Give the component access to its CSS
-import "./styles.css"
-//functional component - must be Capitalised
-import Furnitures from "./Furniture"
-export default function ShowCase(){
-    
-    
-    
-    return(
-        <div className="show-case">
-            <h1>Popular Producs</h1>
-                <div className="box-buttons1">
-                    <div>
-                        <img src="https://www.ikea.com/gb/en/images/products/nordviken-chair-antique-stain__0832454_pe777681_s5.jpg?f=s" alt="image of chair" className="img-box1"  />
-                        <p>Description</p>
-                        <input type="button" value="Add to busket" />
-                    </div>
-                    <div>
-                        <img src="https://www.ikea.com/gb/en/images/products/nordviken-chair-antique-stain__0832454_pe777681_s5.jpg?f=s" alt="image of chair" className="img-box2"  />
-                        <p>Description</p>
-                        <input type="button" value="Add to busket" />
-                    </div>
-                </div>
-                <div className="box-buttons2">
-                    <div>
-                        <img src="https://www.ikea.com/gb/en/images/products/nordviken-chair-antique-stain__0832454_pe777681_s5.jpg?f=s" alt="image of chair" className="img-box1"  />
-                        <p>Description</p>
-                        <input type="button" value="Add to busket" />
-                    </div>
-                    <div>
-                        <img src="https://www.ikea.com/gb/en/images/products/nordviken-chair-antique-stain__0832454_pe777681_s5.jpg?f=s" alt="image of chair" className="img-box2"  />
-                        <p>Description</p>
-                        <input type="button" value="Add to busket" />
-                    </div>
-                </div>
-        </div>
-    )
-}
+import React, { useState } from 'react';
+import "./styles.css";
 
+// Functional component - must be Capitalized
+import Furnitures from "./Furniture";
+
+export default function ShowCase() {
+  
+    return (
+        <div className="show-case">
+            <h1>Popular Products</h1>
+            <div className="box-buttons1">
+                <div>
+                    <img src="https://www.ikea.com/gb/en/images/products/nordviken-chair-antique-stain__0832454_pe777681_s5.jpg?f=s" alt="image of chair" className="img-box1" />
+                    <p>Description</p>
+                    {/* Add onClick event handler to trigger adding item to the basket */}
+                    <input type="button" value="Add to basket" />
+                </div>
+                <div>
+                    <img src="https://www.ikea.com/gb/en/images/products/nordviken-chair-antique-stain__0832454_pe777681_s5.jpg?f=s" alt="image of chair" className="img-box2" />
+                    <p>Description</p>
+                    <input type="button" value="Add to basket"  />
+                </div>
+            </div>
+            <div className="box-buttons2">
+                <div>
+                    <img src="https://www.ikea.com/gb/en/images/products/nordviken-chair-antique-stain__0832454_pe777681_s5.jpg?f=s" alt="image of chair" className="img-box1" />
+                    <p>Description</p>
+                    <input type="button" value="Add to basket" />
+                </div>
+                <div>
+                    <img src="https://www.ikea.com/gb/en/images/products/nordviken-chair-antique-stain__0832454_pe777681_s5.jpg?f=s" alt="image of chair" className="img-box2" />
+                    <p>Description</p>
+                    <input type="button" value="Add to basket" />
+                </div>
+            </div>
+        </div>
+    );
+}
